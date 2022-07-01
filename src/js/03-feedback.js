79966dbc50ca63becf9fdadf4d7fc166c6f57d5a
AlisaLevchenko <good.luck.odria@gmail.com>
2022-07-01 19:05:27 +0300

    Create HW#8 (part 3)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import { throttle } from 'lodash';
 
+const form = document.querySelector('.feedback-form');
+const feedbackInput = document.querySelector('.feedback-form input');
+const feedbackMessage = document.querySelector('.feedback-form textarea');
+
+const formChange = function () {
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({
+      email: feedbackInput.value,
+      message: feedbackMessage.value,
+    })
+  );
+};
+
+const getPreviousData = function () {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (!data) return;
+  feedbackInput.value = data.email;
+  feedbackMessage.value = data.message;
+};
+
+const formSubmit = function (evt) {
+  evt.preventDefault();
+  console.log({
+    email: feedbackInput.value,
+    message: feedbackMessage.value,
+  });
+  form.reset();
+  localStorage.clear();
+};
+
+feedbackInput.addEventListener('input', throttle(formChange, 500));
+feedbackMessage.addEventListener('input', throttle(formChange), 500);
+form.addEventListener('submit', formSubmit);
+
+getPreviousData();
+
+// console.log(inputChange);
